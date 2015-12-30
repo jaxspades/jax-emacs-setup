6eeed96cd6f0dd06b958e9cac47204f892891ae2
Colin Hartwig <chartwig@healthgrades.com>
2015-12-30 15:37:17 -0600

    Changes buffer list to Helm

--- init.el
+++ init.el
@@ -110,6 +110,8 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 
