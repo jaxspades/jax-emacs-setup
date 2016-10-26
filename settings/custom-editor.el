789d6c7b37995974ccfe6de6f3004b9af7f5ac2d
Colin Hartwig <chartwig@healthgrades.com>
2016-10-26 09:45:53 -0500

    Remove unneeded register

--- settings/custom-editor.el
+++ settings/custom-editor.el
@@ -1,2 +1,2 @@
 ;; Custom init file for various machines. This is where I can place any sensitive info that SHOULD NOT be in a public repo.
-(set-register ?a (cons 'file "~/Repositories/salt-master/src/jupiter/src/apps.hospitals-dev.healthgrades.com"))
+
